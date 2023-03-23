756b1e744912f65be6e11f5313f708cb055d0815
Parv singhal <86013119+parvsinghal23@users.noreply.github.com>
2023-03-24 02:11:52 +0530

    fourth commit

--- firewall_security.py
+++ firewall_security.py
@@ -2,9 +2,14 @@ import re
 
 # Define a function to check for security issues in submitted data
 def security_scan(data):
-    # Look for HTML script tags
+    # Look for HTML script tags/cross-site scripting attacks
     if re.search("(\<(script)\>)", data):
         return False
+
+    # Check for common SQL injection attacks
+    if re.search("((\%27)|(\')|(\-\-)|(\%23))[^\n]*((\%27)|(\')|(\-\-)|(\%23))", data):
+        return False
+    
     # Look for various potential attacks using special characters
     elif re.search("((\%3C)|<)[^(\%3E)|>]+((\%3E)|>)", data):
         return False
@@ -22,21 +27,27 @@ def security_scan(data):
         return False
     elif re.search("\|", data):
         return False
-    
+
     elif re.search("[^\w\s]", data):
         return False
-    
+
+    # limits the input data length to 10,000 characters
+    # prevent buffer overflow attacks
+    elif len(data) > 10000:
+        return False
+
     # If no potential security issues are found, return True
     else:
         return True
 
 def security_scan_2(data):
     # Look for proxy address
-    if re.search("(?i)(?<!\d)(?:\d{1,3}\.){3}\d{1,3}(?::\d+)?(?!\d)", data):
+    if re.search(r'(?i)(?<![\d.])(?:\d{1,3}\.){3}\d{1,3}(?::\d+)?(?![\d.])', data):
+        return False
+    # checks that the email address string has a valid format
+    elif not re.search(r'^[a-zA-Z0-9_+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$', data):
         return False
 
     # If no potential security issues are found, return True
     else:
         return True
-
-
